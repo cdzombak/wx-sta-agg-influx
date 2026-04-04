1dad89f411e7ec8449ec813d6aab2234ef447365
Chris Dzombak <chris@dzombak.com>
2026-04-04 15:54:22 -0400

    aggregate rain over the last 24h and 1h

--- rain_agg.go
+++ rain_agg.go
@@ -23,16 +23,44 @@ type RainAggArgs struct {
 	InfluxQueryTimeout time.Duration
 }
 
+const (
+	rainInterval24h = "24h"
+	rainInterval1h  = "1h"
+)
+
+func allRainIntervals() []string {
+	return []string{rainInterval24h, rainInterval1h}
+}
+
+func rainIntervalToDuration(interval string) time.Duration {
+	switch interval {
+	case rainInterval24h:
+		return 24 * time.Hour
+	case rainInterval1h:
+		return time.Hour
+	default:
+		panic(fmt.Sprintf("unknown rain interval: %s", interval))
+	}
+}
+
+func rainResultFieldName(args RainAggArgs, interval string) string {
+	return args.RainField + "_" + interval
+}
+
+type rainDataPoint struct {
+	t    time.Time
+	rain float64
+}
+
 func RainAgg(args RainAggArgs) ([]*influxdb.Point, error) {
 	// note: the given args are assumed to be valid.
 	// if this were a real project or API that other people would use, I'd validate them here.
 
 	tagsWhere := PartialWhereClauseForTags(args.QueryTags)
 
-	// 24h
-	// gather the data we'll need:
-	q := fmt.Sprintf("SELECT time, %s FROM %s WHERE time >= now()-24h %s ORDER BY time ASC",
-		args.RainField, args.MeasurementFrom, tagsWhere)
+	// query for the longest interval; shorter intervals will filter from this data.
+	q := fmt.Sprintf("SELECT time, %s FROM %s WHERE time >= now()-%s %s ORDER BY time ASC",
+		args.RainField, args.MeasurementFrom, rainInterval24h, tagsWhere)
 	log.Printf("[DEBUG] query: %s", q)
 	r, err := args.Influx.Query(influxdb.Query{
 		Command:         q,
@@ -46,7 +74,7 @@ func RainAgg(args RainAggArgs) ([]*influxdb.Point, error) {
 		return nil, fmt.Errorf("InfluxDB query failed: %s", r.Err)
 	}
 	if len(r.Results) == 0 || len(r.Results[0].Series) == 0 {
-		log.Printf("no data to aggregate")
+		log.Printf("no rain data to aggregate")
 		return nil, nil
 	}
 	if len(r.Results) > 1 {
@@ -62,14 +90,13 @@ func RainAgg(args RainAggArgs) ([]*influxdb.Point, error) {
 		return nil, fmt.Errorf("expected second column to be '%s', got '%s'", args.RainField, r.Results[0].Series[0].Columns[1])
 	}
 
-	rainTotal := 0.0
-	prevDataPoint := math.NaN()
-	lastDataPointTime := time.Time{}
+	// parse all data points from the query result:
+	var allData []rainDataPoint
 	for _, sourceDataPoint := range r.Results[0].Series[0].Values {
 		if sourceDataPoint[1] == nil {
 			continue
 		}
-		lastDataPointTime, err = time.Parse(time.RFC3339, sourceDataPoint[0].(string))
+		t, err := time.Parse(time.RFC3339, sourceDataPoint[0].(string))
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse timestamp: %w", err)
 		}
@@ -77,25 +104,60 @@ func RainAgg(args RainAggArgs) ([]*influxdb.Point, error) {
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse rain sensor value: %w", err)
 		}
-		if !math.IsNaN(prevDataPoint) {
-			// skip rollover:
-			if rainSensor < prevDataPoint {
-				prevDataPoint = rainSensor
-				continue
-			}
-			rainTotal += rainSensor - prevDataPoint
-		}
-		prevDataPoint = rainSensor
+		allData = append(allData, rainDataPoint{t: t, rain: rainSensor})
 	}
 
-	p, err := influxdb.NewPoint(
-		args.MeasurementTo,
-		args.WriteTags,
-		map[string]any{
-			args.RainField + "_24h": rainTotal,
-		},
-		lastDataPointTime,
-	)
+	if len(allData) == 0 {
+		log.Printf("no rain data to aggregate")
+		return nil, nil
+	}
 
-	return []*influxdb.Point{p}, err
+	latestTime := allData[len(allData)-1].t
+	var retv []*influxdb.Point
+
+	for _, interval := range allRainIntervals() {
+		dur := rainIntervalToDuration(interval)
+
+		// filter data points for this interval:
+		var intervalData []rainDataPoint
+		for _, dp := range allData {
+			if latestTime.Sub(dp.t) <= dur {
+				intervalData = append(intervalData, dp)
+			}
+		}
+
+		if len(intervalData) == 0 {
+			continue
+		}
+
+		// sum rain, skipping rollovers:
+		rainTotal := 0.0
+		prevDataPoint := math.NaN()
+		lastDataPointTime := intervalData[len(intervalData)-1].t
+		for _, dp := range intervalData {
+			if !math.IsNaN(prevDataPoint) {
+				if dp.rain < prevDataPoint {
+					prevDataPoint = dp.rain
+					continue
+				}
+				rainTotal += dp.rain - prevDataPoint
+			}
+			prevDataPoint = dp.rain
+		}
+
+		p, err := influxdb.NewPoint(
+			args.MeasurementTo,
+			args.WriteTags,
+			map[string]any{
+				rainResultFieldName(args, interval): rainTotal,
+			},
+			lastDataPointTime,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create InfluxDB point: %w", err)
+		}
+		retv = append(retv, p)
+	}
+
+	return retv, nil
 }
